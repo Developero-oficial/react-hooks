2f418975cba5d37df8c3af1994455acb5186bcab
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-11-14 17:12:45 -0600

    added hook example

--- src/use-callback/components/use-callback-page.js
+++ src/use-callback/components/use-callback-page.js
@@ -1,8 +1,45 @@
 import React from 'react';
 
-export const UseCallbackPage = () => (
-  <>
-    <h2>Hook useCallback</h2>
-    <p>Sigue las instrucciones que vienen en el curso.</p>
-  </>
-);
+const Example = ({cb}) => {
+  React.useEffect(() => {
+    console.log('cb changed');
+  }, [cb]);
+
+  return (
+    <ul>
+      <li>true===true //true</li>
+      <li>false===false //true </li>
+      <li>1===1 //true </li>
+      <li>'a'==='a' //true </li>
+      <li>
+        {}==={} //false{' '}
+      </li>
+      <li>[]===[] //false </li>
+      <li>
+        ()=>{}===()=>{} //false{' '}
+      </li>
+      <li>const z={}</li>
+      <li>z===z //true</li>
+    </ul>
+  );
+};
+
+export const UseCallbackPage = () => {
+  const [counter, setCounter] = React.useState(0);
+
+  const handleExample = React.useCallback(() => {}, []);
+
+  const handleClick = () => setCounter(counter + 1);
+
+  return (
+    <>
+      <h2>Hook useCallback</h2>
+      <p>Igualdad referencial en JS:</p>
+
+      <Example cb={handleExample} />
+
+      <button onClick={handleClick}>handle re render</button>
+      <p>re render times: {counter}</p>
+    </>
+  );
+};
