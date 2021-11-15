315079a2e2c3f78af0bc6464becc33ae041c4f1d
juan-carlos-correa <juan.correa.herrera@gmail.com>
2021-11-15 16:47:06 -0600

    hook example

--- src/use-memo/components/use-memo-page.js
+++ src/use-memo/components/use-memo-page.js
@@ -1,8 +1,46 @@
 import React from 'react';
 
-export const UseMemoPage = () => (
-  <>
-    <h2>Hook useMemo</h2>
-    <p>Sigue las instrucciones que vienen en el curso.</p>
-  </>
-);
+const createArray = length => {
+  let i = length;
+  const arr = [];
+  while (i--) {
+    arr[i] = 0;
+  }
+
+  return arr;
+};
+
+const expensiveCalc = myArray => {
+  console.log('expensiveCalc ejecutado');
+  let counter = 0;
+  for (let i = 0; i < myArray.length; i++) {
+    for (let j = 0; j < myArray.length; j++) {
+      counter++;
+    }
+  }
+  return counter;
+};
+
+const MyComponent = ({someList}) => {
+  const result = React.useMemo(() => expensiveCalc(someList), [someList]);
+
+  return <p>Iteraciones: {result.toLocaleString()}</p>;
+};
+
+const list = createArray(10000);
+
+export const UseMemoPage = () => {
+  const [foo, setFoo] = React.useState('');
+
+  const handleChange = ({target}) => setFoo(target.value);
+
+  return (
+    <>
+      <h1>Hook useMemo</h1>
+      <h2>useMemo - Sin aplicar</h2>
+      <p>Escribe para hacer re render.</p>
+      <input value={foo} onChange={handleChange} />
+      <MyComponent someList={list} />
+    </>
+  );
+};
